3cd40c437c149471d266169c6ffee97e729c26c2
babishkarki <babishkarki629@gmail.com>
2025-02-02 01:51:09 +0545

    Responsive issue fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
