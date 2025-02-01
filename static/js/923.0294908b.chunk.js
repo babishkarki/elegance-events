a6f577d667fc890ff25348f82080629ff955d826
babishkarki <babishkarki629@gmail.com>
2025-02-02 01:30:28 +0545

    Updates

---
+++ static/js/923.0294908b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkelegance_events=self.webpackChunkelegance_events||[]).push([[923],{923:(e,n,s)=>{s.r(n),s.d(n,{default:()=>l});s(43);const o="NotFound_container__GSD-B",t="NotFound_homeLink__U-651";var c=s(837),a=s(579);const l=()=>(0,a.jsxs)("div",{className:o,children:[(0,a.jsx)("h1",{children:"404 - Page Not Found"}),(0,a.jsx)("p",{children:"The page you're looking for doesn't exist"}),(0,a.jsx)(c.N_,{to:"/",className:t,children:"Return to Home"})]})}}]);
+//# sourceMappingURL=923.0294908b.chunk.js.map
